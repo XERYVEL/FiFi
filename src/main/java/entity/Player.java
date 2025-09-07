8f0fba5c703813acd2ec383bdde3789004b11a91
XERYVEL <julianfer.ar@gmail.com>
2025-09-07 12:45:31 -0300

    clase3

--- src/main/java/entity/Player.java
+++ src/main/java/entity/Player.java
@@ -1,4 +1,84 @@
 package entity;
 
-public class Player {
+import main.gamePanel;
+import main.KeyHandler;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+public class Player extends Entity{
+    gamePanel gp;
+    KeyHandler keyH;
+
+    public Player (gamePanel gp, KeyHandler keyH) {
+        this.gp = gp;
+        this.keyH = keyH;
+
+        setDefaultValues() ;
+        getPlayersImage();
+    }
+    public void setDefaultValues() {
+
+        x = 100;
+        y = 100;
+        speed = 4;
+        direction = "right";
+    }
+    //Metodo para usar las imagenes
+    public void getPlayersImage(){
+        try{
+
+            up1 = ImageIO.read(getClass().getResourceAsStream("/player/Arriba_1.png"));
+            up2 = ImageIO.read(getClass().getResourceAsStream("/player/Arriba_2.png"));
+            down1 = ImageIO.read(getClass().getResourceAsStream("/player/Abajo_1.png"));
+            down2 = ImageIO.read(getClass().getResourceAsStream("/player/Abajo_2.png"));
+            left1 = ImageIO.read(getClass().getResourceAsStream("/player/Izquiera_1.png"));
+            left2 = ImageIO.read(getClass().getResourceAsStream("/player/Izquierda_2.png"));
+            right1 = ImageIO.read(getClass().getResourceAsStream("/player/Derecha_1.png"));
+            right2 = ImageIO.read(getClass().getResourceAsStream("/player/Derecha_2.png"));
+
+        }catch(IOException e) {
+            e.printStackTrace();
+        }
+    }
+    //Metodo para mandar los controles del jugador
+    public void update() {
+        if(keyH.upPressed == true) {
+            direction = "Arriba";
+            y -= speed;
+        }
+        else if(keyH.downPressed == true) {
+            direction = "Abajo";
+            y += speed;
+        }
+        else if(keyH.leftPressed == true) {
+            direction = "Izquierda";
+            x -= speed;
+        }
+        else if(keyH.rightPressed == true) {
+            direction = "Derecha";
+            x += speed;
+        }
+    }
+    public void draw(Graphics2D g2) {
+
+        BufferedImage image = null;
+        switch(direction) {
+            case "Arriba":
+                image = up1;
+                break;
+            case "Abajo":
+                image = down1;
+                break;
+            case "Izquierda":
+                image = left1;
+                break;
+            case "Derecha":
+                image = right1;
+                break;
+        }
+        g2.drawImage(image, x, y, gp.tileSize, gp.tileSize, null);
+    }
 }
--- src/main/java/main/gamePanel.java
+++ src/main/java/main/gamePanel.java
@@ -1,5 +1,6 @@
 package main;
 
+import entity.Player;
 import org.w3c.dom.ls.LSOutput;
 
 import javax.swing.JPanel;
@@ -10,7 +11,7 @@ public class gamePanel extends JPanel implements Runnable {
     final int originalTileSize = 16; // Tamaño de pixeles para los personajes y objetos(?
     final int scale = 3;
 
-    final int tileSize = originalTileSize * scale; // escala a 48x48
+    public int tileSize = originalTileSize * scale; // escala a 48x48
     final int maxScreenCol = 16;
     final int maxScreenRow = 12;
     final int screenWidth = tileSize * maxScreenCol; // 768 pixeles
@@ -23,6 +24,7 @@ public class gamePanel extends JPanel implements Runnable {
 
     KeyHandler keyH = new KeyHandler();
     Thread gameThread;
+    Player player =new Player(this, keyH);
 
     int playerX = 100;
     int playerY = 100;
@@ -74,19 +76,11 @@ public class gamePanel extends JPanel implements Runnable {
         }
 
     }
+
     public void update() {
-        if(keyH.upPressed == true) {
-            playerY -= playerSpeed;
-        }
-        else if(keyH.downPressed == true) {
-            playerY += playerSpeed;
-        }
-        else if(keyH.leftPressed == true) {
-            playerX -= playerSpeed;
-        }
-        else if(keyH.rightPressed == true) {
-            playerX += playerSpeed;
-        }
+
+        //Llamamos el metodo update del objeto player
+        player.update();
     }
     public void paintComponent(Graphics g) {
 
@@ -94,8 +88,9 @@ public class gamePanel extends JPanel implements Runnable {
 
         Graphics2D g2 =(Graphics2D)g;
 
-        g2.setColor(Color.white);
-        g2.fillRect(playerX, playerY, tileSize, tileSize);
+        //Llamamos el metodo draw del objeto player
+        player.draw(g2);
+
         g2.dispose();
 
     }
